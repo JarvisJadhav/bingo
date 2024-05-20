ff43285bdce3894c66a7bc7f3cf6a9400e6f1382
praveenjadhav1510 <155697218+praveenjadhav1510@users.noreply.github.com>
2024-05-20 21:13:55 +0530

    Add files via upload

--- gamescript.js
+++ gamescript.js
@@ -4,7 +4,8 @@ let count = 0;
 var p = [1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10 ,11 ,12 ,13 ,14 ,15 ,16 ,17 ,18 ,19 ,20 ,21 ,22 ,23 ,24 ,25];
 let buttonValue = 0;
 var bi = document.getElementById('print');
-//timing function
+var rbt = document.getElementById('resetbt');
+
 async function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -30,14 +31,9 @@ async function sleep(ms) {
             clicked.push(buttonValue);
             checkwinner();
             button.style.backgroundColor = '#B0BEC5';
-            if(count==5){
-                bi.style.scale = '1.8';
-                for(var i=10; i>=0; i--){
-                    document.getElementById('print').textContent ="BINGO reset's in "+i+"s";
-                    await sleep(1000);
-                }
-                window.location.href = 'index.html';
-            }
+            /*if(count==5){
+                document.getElementById('print').textContent ="count 5 Bingo";
+            }*/
          }
     }
 
@@ -70,7 +66,10 @@ async function sleep(ms) {
         if(count==3){ bi.innerText = "count 3"}
         if(count==4){ bi.innerText = "count 4"}
 
-        if(count !== 5){ count=0; }
+        if(count < 5){ count=0; }
+        if(count > 4){
+            document.getElementById('print').textContent ="count 5 Bingo";
+        }
     }
 
     var bt = document.getElementsByClassName('b');
@@ -78,40 +77,73 @@ async function sleep(ms) {
         for (var i = 0; i < bt.length; i++) {
             bt[i].style.color = 'cyan';
             bt[i].style.boxShadow = 'inset 0px 0px 2px cyan';
-        }
-        bi.style.color = "cyan";
+        } themeColor('cyan');
     });
     
     document.getElementById('c2').addEventListener('click', function () {
         for (var i = 0; i < bt.length; i++) {
             bt[i].style.color = 'red';
             bt[i].style.boxShadow = 'inset 0px 0px 2px red';
-        }
-        bi.style.color = "red";
+        } themeColor('red');
     });
     
     document.getElementById('c3').addEventListener('click', function () {
         for (var i = 0; i < bt.length; i++) {
-            bt[i].style.color = '#CC00FF';
-            bt[i].style.boxShadow = 'inset 0px 0px 2px #CC00FF';
-        }
-        bi.style.color = "#CC00FF";
+            bt[i].style.color = '#9900ff';
+            bt[i].style.boxShadow = 'inset 0px 0px 2px #9900ff';
+        } themeColor('#9900ff');
     });
     
     document.getElementById('c4').addEventListener('click', function () {
         for (var i = 0; i < bt.length; i++) {
             bt[i].style.color = '#33FF99';
             bt[i].style.boxShadow = 'inset 0px 0px 2px #33FF99';
-        }
-        bi.style.color = "#33FF99";
+        } themeColor('#33FF99');
     });
     
     document.getElementById('c5').addEventListener('click', function () {
         for (var i = 0; i < bt.length; i++) {
             bt[i].style.color = '#607D8B';
             bt[i].style.boxShadow = 'inset 0px 0px 2px #607D8B';
-        }
-        bi.style.color = "#607D8B";
+        } themeColor('#607D8B');
     });
+    function themeColor(color) {
+        bi.style.color = color;
+        rbt.style.backgroundColor = color;
+    }
+    function reset() {
+        myArray = [];
+        clicked = [];
+        count = 0;
+        p = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+        buttonValue = 0;
     
+        const buttonElements = document.querySelectorAll('table#numberTable button');
+        buttonElements.forEach(button => {
+            button.style.backgroundColor = '';
+            button.textContent = '';
+        });
+        bi.style.scale = '1';
+        bi.textContent = 'created by Jarvis';
+    
+        generateRandomNumbers();
+    }    
+    
+	const b = document.querySelectorAll('button');
+	b.forEach(btn => {
+		btn.addEventListener('click',function(e){
+			let x = e.clientX - e.target.offsetLeft;
+			let y = e.clientY - e.target.offsetTop;
+			let r = document.createElement('span');
+			r.style.left = x+'px';
+			r.style.top = y+'px';
+			this.appendChild(r);
+
+			setTimeout(()=> {
+				r.remove();
+			},1500);
+		});
+	});
+
 generateRandomNumbers();
+
